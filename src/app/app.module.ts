861a39ffaa5e8edf04bb123941fc874d512eaf51
Christopher Goulart <cgoulart35@gmail.com>
2020-12-10 20:59:31 -0500

    Added basic functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
-import { AppComponent } from './app.component';
+import { CgWebAppComponent } from './cg-web-app.component';
+import { NavBarComponent } from './nav/nav-bar.component';
+import { EventsListComponent } from './events/events-list.component';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
+import { CreateEventComponent } from './events/create-event.component';
+import { EventDetailsComponent } from './events/event-details/event-details.component';
+
+import { EventService } from './events/shared/event.service';
+import { ToastrService } from './common/toastr.service';
+
+import { appRoutes } from './routes'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    CgWebAppComponent,
+    NavBarComponent,
+    EventsListComponent,
+    EventThumbnailComponent,
+    CreateEventComponent,
+    EventDetailsComponent
+  ],
+  providers: [EventService, ToastrService],
+  bootstrap: [CgWebAppComponent]
 })
 export class AppModule { }
